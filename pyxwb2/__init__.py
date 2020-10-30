e0120051f6737d4d980ad5c83820eb51790beb99
Dennis Whitney <dennis@irunasroot.com>
2020-10-29 22:13:07 -0500

    Added purple maneuver to dial conversion

--- pyxwb2/__init__.py
+++ pyxwb2/__init__.py
@@ -4,6 +4,6 @@ __version__ = "0.2.0b"
 __vendors__ = {
     "xws": "2.0",
     "xwing-data": "1.25.0",
-    "dial-maneuvers-conversion": "1.0.0",
+    "dial-maneuvers-conversion": "1.0.1",
     "jsonschema": "2019-09"
 }
